f5402670c4de3bc33ce7961c00f9eb6db2b28553
232016 <232016@DESKTOP-L8QUMJ2>
2022-10-22 14:14:32 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Components/BDConnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Components
+{
+    internal class BDConnect
+    {
+        public static Service1Entities db = new Service1Entities();
+
+    }
+}
