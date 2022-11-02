7997849de56aff1db3bc19e5c65a910d273097cb
cbvfgtn217@gmail.com <cbvfgtn217@gmail.com>
2022-11-02 09:26:24 +0300

    2

--- WpfApp1/Components/PartialClass/Service.cs
+++ WpfApp1/Components/PartialClass/Service.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace WpfApp1.Components
 {
     public partial class Service
     {
+        public string StrDiscount
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "";
+                else
+                    return $"* скидка {Discount} %";
+            }
+        }
+        public string CostDuration
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return $"{Cost:F} рублей за  {DurationInSeconds / 60} минут";
+                else
+                    return $" {(double)Cost - (double)Cost * (double)Discount/100:F} рублей за {DurationInSeconds/60} минут";
+            }
+        }
+        public Visibility DiscountVisability
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+
+        }
+        public string ColorDis
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "#ffffff";
+                else
+                    return "#D1FFD1";
+            }
+        }
     }
 }
